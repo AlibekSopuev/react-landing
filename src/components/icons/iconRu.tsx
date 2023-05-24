8fc2c7cac4f0709f6a6338f3de28d4c2596c2ad3
alibeksopuev <alibek@360fin.tech>
2023-05-24 18:56:40 +0600

    icons

--- src/components/icons/iconRu.tsx
+++ src/components/icons/iconRu.tsx
@@ -2,13 +2,21 @@ import { SvgPropsTypes } from './types';
 
 export const IconRu = (props: SvgPropsTypes) => (
   <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <mask id='mask0_5829_9398' maskUnits='userSpaceOnUse' x='0' y='3' width='24' height='18'>
+    <mask
+      id='mask0_5864_9398'
+      style={{ maskType: 'alpha' }}
+      maskUnits='userSpaceOnUse'
+      x='0'
+      y='3'
+      width='24'
+      height='18'
+    >
       <path
         d='M12 20.9996C9.0071 20.9991 6.01711 20.8187 3.04598 20.4596C1.70985 20.2831 0.652635 19.2423 0.455036 17.9093C-0.151679 13.9921 -0.151679 10.0048 0.455036 6.0876C0.652635 4.75458 1.70985 3.71385 3.04598 3.53731C8.99373 2.8209 15.0061 2.8209 20.9541 3.53731C22.29 3.71385 23.3472 4.75458 23.5448 6.0876C24.1517 10.0048 24.1517 13.9921 23.5448 17.9093C23.3472 19.2423 22.29 20.2831 20.9541 20.4596C17.9828 20.8187 14.9928 20.9991 12 20.9996Z'
         fill='#EA0000'
       />
     </mask>
-    <g mask='url(#mask0_5829_9398)'>
+    <g mask='url(#mask0_5864_9398)'>
       <rect x='-0.750977' y='3' width='25.5' height='6' fill='#F0F0F0' />
       <rect x='-0.750977' y='9' width='25.5' height='6' fill='#0052B4' />
       <rect x='-0.750977' y='15' width='25.5' height='6' fill='#D80027' />
