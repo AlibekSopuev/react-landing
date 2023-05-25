bfe67c96376a4aa7c932fedf2837a1235846b022
alibeksopuev <alibek@360fin.tech>
2023-05-25 21:08:58 +0600

    fix main

--- src/components/shared/header/const.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import General from './variants/general';
-import { HeaderVariantProps, HeaderVariants } from './types';
-
-export const HEADER_VARIANTS: { [variant in HeaderVariants]: React.FC<HeaderVariantProps> } = {
-  general: General,
-};
--- src/features/main/banner/styled.ts
+++ src/features/main/banner/styled.ts
@@ -1,5 +1,4 @@
 import styled, { css } from 'styled-components';
-import { IconBanner } from '../../../components/icons/iconBanner';
 
 export const Wrapper = styled.div`
   display: flex;
